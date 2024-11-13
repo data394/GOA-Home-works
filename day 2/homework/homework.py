1fd164b91a3b7be552e906f74ca530d66683a96f
data394 <datagelashvili22@btu.edu.ge>
2024-11-13 07:10:46 +0400

    second commit

---
+++ day 2/homework/homework.py
@@ -0,0 +1,28 @@
+name=("Data ")
+
+surname=("gelashvili")
+
+age=(" 12")
+
+hobby=("play football")
+
+contry=("Georgia")
+
+print(name+surname)
+print("i am"+age)
+print("my hobby is to "+hobby)
+print("i am from "+contry)
+
+
+
+
+print(3+3)
+print(10-2)
+print(5*5)
+print(9*9)
+print(5+5)
+print(4/2)
+print(7+7)
+print(9/3)
+print(10**2)
+print(7**3)
