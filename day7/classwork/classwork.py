13b1a9394e2d16dc1df87de776d8acf54cd769b9
data394 <datagelashvili22@btu.edu.ge>
2024-11-16 05:13:52 +0400

    seventh commit

---
+++ day7/classwork/classwork.py
@@ -0,0 +1,11 @@
+name=input("enter youre name: ")
+password=input("enter youre pasword: ")
+
+
+
+num1=int(input("enter youre first number: "))
+num2=int(input("enter youre seccend number: "))
+print(num1*num2)
+print(num1/num2)
+print(num1+num2)
+print(num1-num2)
---
+++ day7/homework/homework.py
@@ -0,0 +1,25 @@
+name=("david")
+age=("12")
+username=(" gelashvili")
+print(name+ username+" is "+age)
+#თქვენი სახელი გვარი და ასაკი
+
+
+#კალკულაცია
+num1=int(input("enter youre first number: "))
+num2=int(input("enter youre seccend number: "))
+print(num1*num2)
+print(num1/num2)
+print(num1+num2)
+print(num1-num2)
+
+
+#ემაილის შექმნა
+print=input("enter youre email: ")
+print=input("enter youre email pasword: ")
+
+
+
+salary = 900
+new_salary = salary + 200
+print(new_salary)
