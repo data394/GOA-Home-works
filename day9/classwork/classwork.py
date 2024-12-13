5c4cc58103d14be98446154f7b5092e760318692
data394 <datagelashvili22@btu.edu.ge>
2024-12-13 05:30:18 +0400

    ninth commitµ

---
+++ day9/classwork/classwork.py
@@ -0,0 +1,20 @@
+a=float(input("what is your number"))
+b=float(input("what is your number"))
+
+
+
+
+
+
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b)
+
+
+
+
+#ინტენჯერი არის მთელი რიცხვი და წილადი აი ასეთი არის ფლოათი მაგალითად 8.3
+
+
+
---
+++ day9/homework/homework.py
@@ -0,0 +1,16 @@
+#მომხმარებლის დაბადების თარიღი
+a=int(input("enter youre birth"))
+
+print(a)
+
+#hello (username)
+b=str(input("enter youre username"))
+
+print("hello "+ b)
+
+
+#int არის მთელი რიცხვი
+
+#str არის რაიმე წინადადება ას სიტყვა
+
+#მონაცემთა ტიპები არსებობს float  string  intenger
